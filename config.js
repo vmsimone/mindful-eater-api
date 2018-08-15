f3d7d944681d7333dfa7d4c9174b4b6a3450a60e
Vincent Simone <vsimone@protonmail.com>
2018-08-14 20:28:47 -0700

    added API_BASE_URL to config

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 'use strict';
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-Food';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/Food';
-exports.PORT = process.env.PORT || 8080;
+module.exports = {
+    PORT: process.env.PORT || 8080,
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/Food',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-Food',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
+};
